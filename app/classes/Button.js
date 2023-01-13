f87ac5996d5cc4a3a04a4d8f46cd8fa32925937d
Marcos Sanchez <msanchez@hircasa.com>
2023-01-12 21:10:03 -0600

    hubicando donde cambiar background, fotos y tamaño de texto

--- app/classes/Button.js
+++ app/classes/Button.js
@@ -1,49 +1,44 @@
-import GSAP from 'gsap';
+import GSAP from "gsap";
 
-import Component from 'classes/Component';
+import Component from "classes/Component";
 
 export default class Button extends Component {
   constructor({ element }) {
     super({ element });
-
-    this.path = element.querySelector('path:last-child');
-    this.pathLength = this.path.getTotalLength();
-
-    // console.log(this.path.getTotalLength());
-
-    this.timeline = GSAP.timeline({ paused: true });
-
-    this.timeline.fromTo(
-      this.path,
-      {
-        strokeDashoffset: this.pathLength,
-        strokeDasharray: `${this.pathLength} ${this.pathLength}`,
-      },
-      {
-        strokeDashoffset: 0,
-        strokeDasharray: `${this.pathLength} ${this.pathLength}`,
-      }
-    );
+    // this.path = element.querySelector('path:last-child');
+    // this.pathLength = this.path.getTotalLength();
+    // this.timeline = GSAP.timeline({ paused: true });
+    // this.timeline.fromTo(
+    //   this.path,
+    //   {
+    //     strokeDashoffset: this.pathLength,
+    //     strokeDasharray: `${this.pathLength} ${this.pathLength}`,
+    //   },
+    //   {
+    //     strokeDashoffset: 0,
+    //     strokeDasharray: `${this.pathLength} ${this.pathLength}`,
+    //   }
+    // );
   }
 
   onMouseEnter() {
-    this.timeline.play();
+    // this.timeline.play();
   }
 
   onMouseLeave() {
-    this.timeline.reverse();
+    // this.timeline.reverse();
   }
 
   addEventListeners() {
     this.onMouseEnterEvent = this.onMouseEnter.bind(this);
     this.onMouseLeaveEvent = this.onMouseLeave.bind(this);
 
-    this.element.addEventListener('mouseenter', this.onMouseEnterEvent);
-    this.element.addEventListener('mouseleave', this.onMouseLeaveEvent);
+    this.element.addEventListener("mouseenter", this.onMouseEnterEvent);
+    this.element.addEventListener("mouseleave", this.onMouseLeaveEvent);
   }
 
   removeEventListeners() {
-    this.element.removeEventListener('mouseenter', this.onMouseEnterEvent);
-    this.element.removeEventListener('mouseleave', this.onMouseLeaveEvent);
+    this.element.removeEventListener("mouseenter", this.onMouseEnterEvent);
+    this.element.removeEventListener("mouseleave", this.onMouseLeaveEvent);
   }
 }
