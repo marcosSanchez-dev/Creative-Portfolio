25cf7e0402dc362a2f3ca05a65fe159336ce6780
Marcos Sanchez <msanchez@hircasa.com>
2023-01-21 13:14:19 -0600

    Home done. Missing links to social media

--- app/index.js
+++ app/index.js
@@ -1,20 +1,20 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-new */
 
-import NormalizeWheel from 'normalize-wheel';
+import NormalizeWheel from "normalize-wheel";
 
-import each from 'lodash/each';
+import each from "lodash/each";
 
-import Canvas from 'components/Canvas';
-import Detection from 'classes/Detection';
+import Canvas from "components/Canvas";
+import Detection from "classes/Detection";
 
-import Navigation from 'components/Navigation';
-import Preloader from 'components/Preloader';
+import Navigation from "components/Navigation";
+import Preloader from "components/Preloader";
 
-import About from 'pages/About';
-import Collections from 'pages/Collections';
-import Home from 'pages/Home';
-import Detail from 'pages/Detail';
+import About from "pages/About";
+import Collections from "pages/Collections";
+import Home from "pages/Home";
+import Detail from "pages/Detail";
 
 class App {
   constructor() {
@@ -44,7 +44,7 @@ class App {
       canvas: this.canvas,
     });
 
-    this.preloader.once('completed', this.onPreloaded.bind(this));
+    this.preloader.once("completed", this.onPreloaded.bind(this));
   }
 
   createCanvas() {
@@ -54,8 +54,8 @@ class App {
   }
 
   createContent() {
-    this.content = document.querySelector('.content');
-    this.template = this.content.getAttribute('data-template');
+    this.content = document.querySelector(".content");
+    this.template = this.content.getAttribute("data-template");
   }
 
   createPages() {
@@ -90,43 +90,56 @@ class App {
   }
 
   async onChange({ url, push = true }) {
-    this.canvas.onChangeStart(this.template, url);
+    console.log("pathname: ", window.location.pathname);
+    console.log("window.location: ", window.location);
+    console.log("url: ", url);
+    console.log("url == /: ", url == "/");
+    console.log("url includes About: ", url.includes("/about"));
+    console.log("url pathname === Home: ", window.location.pathname == "/");
 
-    await this.page.hide();
+    if (url == "http://localhost:8004/" || url.includes("/about")) {
+      this.canvas.onChangeStart(this.template, url);
 
-    const res = await window.fetch(url);
+      await this.page.hide();
 
-    if (res.status === 200) {
-      const html = await res.text();
-      const div = document.createElement('div');
+      const res = await window.fetch(url);
 
-      if (push) {
-        window.history.pushState({}, '', url);
+      if (res.status === 200) {
+        const html = await res.text();
+        const div = document.createElement("div");
+
+        if (push) {
+          window.history.pushState({}, "", url);
+        }
+
+        div.innerHTML = html;
+
+        const divContent = div.querySelector(".content");
+
+        this.template = divContent.getAttribute("data-template");
+
+        this.navigation.onChange(this.template);
+
+        this.content.setAttribute("data-template", this.template);
+        this.content.innerHTML = divContent.innerHTML;
+
+        this.canvas.onChangeEnd(this.template);
+
+        this.page = this.pages[this.template];
+        this.page.create();
+
+        this.onResize();
+
+        this.page.show();
+
+        this.addLinkListeners();
+      } else {
+        console.error(`response status: ${res.status}`);
       }
-
-      div.innerHTML = html;
-
-      const divContent = div.querySelector('.content');
-
-      this.template = divContent.getAttribute('data-template');
-
-      this.navigation.onChange(this.template);
-
-      this.content.setAttribute('data-template', this.template);
-      this.content.innerHTML = divContent.innerHTML;
-
-      this.canvas.onChangeEnd(this.template);
-
-      this.page = this.pages[this.template];
-      this.page.create();
-
-      this.onResize();
-
-      this.page.show();
-
-      this.addLinkListeners();
     } else {
-      console.error(`response status: ${res.status}`);
+      window.open(`${url}`, "_blank");
+
+      // window.location.assign(`${url}`);
     }
   }
 
@@ -193,21 +206,21 @@ class App {
    */
 
   addEventListeners() {
-    window.addEventListener('mousewheel', this.onWheel.bind(this));
+    window.addEventListener("mousewheel", this.onWheel.bind(this));
 
-    window.addEventListener('mousedown', this.onTouchDown.bind(this));
-    window.addEventListener('mousemove', this.onTouchMove.bind(this));
-    window.addEventListener('mouseup', this.onTouchUp.bind(this));
+    window.addEventListener("mousedown", this.onTouchDown.bind(this));
+    window.addEventListener("mousemove", this.onTouchMove.bind(this));
+    window.addEventListener("mouseup", this.onTouchUp.bind(this));
 
-    window.addEventListener('touchstart', this.onTouchDown.bind(this));
-    window.addEventListener('touchmove', this.onTouchMove.bind(this));
-    window.addEventListener('touchend', this.onTouchUp.bind(this));
+    window.addEventListener("touchstart", this.onTouchDown.bind(this));
+    window.addEventListener("touchmove", this.onTouchMove.bind(this));
+    window.addEventListener("touchend", this.onTouchUp.bind(this));
 
-    window.addEventListener('resize', this.onResize.bind(this));
+    window.addEventListener("resize", this.onResize.bind(this));
   }
 
   addLinkListeners() {
-    const links = document.querySelectorAll('a');
+    const links = document.querySelectorAll("a");
 
     each(links, (link) => {
       link.onclick = (event) => {
