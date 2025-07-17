880d36b48fb2bf5503a0675e8c89ac295de6fd13
Marcos <marcos.sanchez@saei.mx>
2025-07-16 20:32:31 -0600

    optimizing preloader

--- app/components/Preloader.js
+++ app/components/Preloader.js
@@ -1,119 +1,154 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-undef */
-import { Texture } from 'ogl';
-
-import GSAP from 'gsap';
-
-import Component from 'classes/Component';
-
-import each from 'lodash/each';
-
-import { split } from 'utils/text';
+import { Texture } from "ogl";
+import GSAP from "gsap";
+import Component from "classes/Component";
+import { split } from "utils/text";
 
 export default class Preloader extends Component {
   constructor({ canvas }) {
     super({
-      element: '.preloader',
+      element: ".preloader",
       elements: {
-        title: '.preloader__text',
-        number: '.preloader__number',
-        numberText: '.preloader__number__text',
+        title: ".preloader__text",
+        number: ".preloader__number",
+        numberText: ".preloader__number__text",
       },
     });
 
     this.canvas = canvas;
+    this.textures = {};
+    this.assetCount = window.ASSETS.length;
+    this.loadedCount = 0;
+    this.welcomeMessageShown = false; // Control para el mensaje de bienvenida
 
-    window.TEXTURES = {};
-
+    // Mantener la doble división para el mensaje de bienvenida
     split({
       element: this.elements.title,
-      expression: '<br>',
+      expression: "<br>",
     });
 
     split({
       element: this.elements.title,
-      expression: '<br>',
+      expression: "<br>",
     });
 
     this.elements.titleSpans =
-      this.elements.title.querySelectorAll('span span');
+      this.elements.title.querySelectorAll("span span");
 
-    this.length = 0;
-
-    this.createLoader();
+    this.loadAssets();
   }
 
-  createLoader() {
-    each(window.ASSETS, (image) => {
-      const texture = new Texture(this.canvas.gl, {
-        generateMipmaps: false,
-      });
+  async loadAssets() {
+    const loadPromises = window.ASSETS.map((image) => this.loadTexture(image));
 
-      const media = new window.Image();
-
-      media.crossOrigin = 'anonymous';
-      media.src = image;
-
-      media.onload = (_) => {
-        texture.image = media;
-
-        this.onAssetLoaded();
-      };
-
-      window.TEXTURES[image] = texture;
-    });
-  }
-
-  onAssetLoaded(image) {
-    this.length++;
-
-    const percent = this.length / window.ASSETS.length;
-
-    this.elements.numberText.innerHTML = `${Math.round(percent * 100)}%`;
-
-    if (percent === 1) {
+    try {
+      await Promise.all(loadPromises);
+      this.showWelcomeMessage(); // Mostrar mensaje de bienvenida
+      this.onLoaded();
+    } catch (error) {
+      console.error("Error loading assets:", error);
+      this.showWelcomeMessage(); // Mostrar mensaje incluso con errores
       this.onLoaded();
     }
   }
 
-  onLoaded() {
-    return new Promise((resolve) => {
-      this.emit('completed');
+  loadTexture(imageSrc) {
+    return new Promise((resolve, reject) => {
+      const texture = new Texture(this.canvas.gl, {
+        generateMipmaps: false,
+        anisotropic: 16,
+      });
+
+      const media = new Image();
+      media.crossOrigin = "anonymous";
+      media.src = imageSrc;
+
+      media.onload = () => {
+        texture.image = media;
+        this.textures[imageSrc] = texture;
+        this.onAssetLoaded();
+        resolve();
+      };
+
+      media.onerror = (error) => {
+        console.warn(`Failed to load image: ${imageSrc}`, error);
+        this.onAssetLoaded();
+        resolve(); // Continuar aunque falle
+      };
+    });
+  }
+
+  onAssetLoaded() {
+    this.loadedCount++;
+    const percent = Math.round((this.loadedCount / this.assetCount) * 100);
+    this.elements.numberText.textContent = `${percent}%`;
+  }
+
+  showWelcomeMessage() {
+    if (!this.welcomeMessageShown) {
+      this.welcomeMessageShown = true;
+
+      // Animación para mostrar el mensaje de bienvenida
+      GSAP.fromTo(
+        this.elements.titleSpans,
+        {
+          autoAlpha: 0,
+          y: "20%",
+        },
+        {
+          autoAlpha: 1,
+          y: "0%",
+          duration: 1.5,
+          stagger: 0.1,
+          ease: "expo.out",
+        }
+      );
+    }
+  }
+
+  async onLoaded() {
+    window.TEXTURES = this.textures;
+
+    await new Promise((resolve) => {
+      this.emit("completed");
 
       this.animateOut = GSAP.timeline({
         delay: 1,
-      });
-
-      this.animateOut.to(this.elements.titleSpans, {
-        duration: 1.5,
-        ease: 'expo.out',
-        stagger: 0.1,
-        y: '150%',
-      });
-
-      this.animateOut.to(
-        this.elements.numberText,
-        {
-          duration: 1.5,
-          ease: 'expo.out',
-          stagger: 0.1,
-          y: '100%',
+        onComplete: () => {
+          this.destroy();
+          resolve();
         },
-        '-=1.4'
-      );
-
-      this.animateOut.to(this.element, {
-        autoAlpha: 0,
-        duration: 1.5,
       });
 
-      this.animateOut.call((_) => {
-        this.destroy();
-      });
+      this.animateOut
+        .to(this.elements.titleSpans, {
+          duration: 1.5,
+          ease: "expo.out",
+          stagger: 0.1,
+          y: "150%",
+        })
+        .to(
+          this.elements.numberText,
+          {
+            duration: 1.5,
+            ease: "expo.out",
+            y: "100%",
+          },
+          "-=1.4"
+        )
+        .to(
+          this.element,
+          {
+            autoAlpha: 0,
+            duration: 1.5,
+          },
+          "-=0.1"
+        );
     });
   }
 
   destroy() {
-    this.element.parentNode.removeChild(this.element);
+    if (this.element.parentNode) {
+      this.element.parentNode.removeChild(this.element);
+    }
   }
 }
